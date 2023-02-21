b5f5ba7f11ea2ca9cc8326c1add1a93451b2ec09
Karinaguseva <karinguseva@gmail.com>
2023-02-21 11:38:52 +0300

    feat: add switcher for changing theme

--- frontend/src/init.tsx
+++ frontend/src/init.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { AuthProvider } from './context/authContext';
+import { ThemeProvider } from './context/themeContext';
 import store from './slice/store/store';
 
 const init = () => {
@@ -11,9 +12,11 @@ const init = () => {
   root.render(
     <React.StrictMode>
       <AuthProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
+        <ThemeProvider>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </ThemeProvider>
       </AuthProvider>
     </React.StrictMode>,
   );
